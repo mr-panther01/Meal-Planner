ae6245cd9b244babf0e652a139ea818c7163bc6b
mr-panther01 <adityasharma92006@gmail.com>
2025-04-21 16:33:20 +0530

    try fix deploy

--- vite.config.js
+++ vite.config.js
@@ -5,23 +5,4 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-  server: {
-    port: 5173,
-    strictPort: true,
-  },
-  preview: {
-    port: 4173,
-    strictPort: true,
-  }
 })
